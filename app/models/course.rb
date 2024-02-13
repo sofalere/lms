88c850a2effc0cfe8d3af23ea31be9de53d022d6
sofalere <sofalere@gmail.com>
2024-02-13 08:31:12 -0600

    1.5 hours into instructions

---
+++ app/models/course.rb
@@ -0,0 +1,8 @@
+class Course < ApplicationRecord
+  has_one_attached :image do |attachable|
+    attachable.variant :thumb, resize_to_limit: [100, 100]
+  end
+
+  has_many :lessons
+  has_and_belongs_to_many :categories
+end
---
+++ app/models/lesson.rb
@@ -0,0 +1,4 @@
+class Lesson < ApplicationRecord
+  # could attach video or image 
+  belongs_to :course
+end
